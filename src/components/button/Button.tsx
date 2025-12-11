bde8e35db7cf0e7f916725adda399985d6774f2b
Natasa Subic <natasa.subic94@gmail.com>
2025-12-11 20:29:44 +0100

    feature: style button component

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,9 +1,10 @@
 import React, { type ButtonHTMLAttributes } from 'react';
+import { StyledButton } from './Button.styled';
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
 }
 
 export const Button: React.FC<ButtonProps> = ({ children, ...props }) => {
-  return <button {...props}>{children}</button>;
+  return <StyledButton {...props}>{children}</StyledButton>;
 };
